3d112948838e1306be1a31d4b562fc87a2fe407c
lachlan <lachlan.miller.1990@outlook.com>
2017-11-23 20:41:32 +0900

    Remove unused style

--- client/App.js
+++ client/App.js
@@ -4,18 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Welcome to the Gym App</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
