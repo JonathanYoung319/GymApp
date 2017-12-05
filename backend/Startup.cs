153e90beecabf70e1ef1c2c42f36f2e665551433
lachlan <lachlan.miller.1990@outlook.com>
2017-12-05 19:55:15 +0900

    Spacing

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -12,34 +12,34 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 namespace backend
 {
-		public class Startup
-		{
-				public Startup(IConfiguration configuration)
-				{
-						Configuration = configuration;
-				}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-				public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-				// This method gets called by the runtime. Use this method to add services to the container.
-				public void ConfigureServices(IServiceCollection services)
-				{
-						services.AddMvc();
-				}
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
 
-				// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-				public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-				{
-						if (env.IsDevelopment())
-						{
-								app.UseDeveloperExceptionPage();
-						}
-						app.UseForwardedHeaders(new ForwardedHeadersOptions
-										{
-										ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-										});
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
-						app.UseMvc();
-				}
-		}
+            app.UseMvc();
+        }
+    }
 }
